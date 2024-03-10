3e1f96d1ec413842a0b4b5f1006a64aa30fd6925
Gabor Torok <gabor@20y.hu>
2024-03-10 08:31:48 +0100

    Use key arrows to navigate the game

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,8 @@
 module Main exposing (init, main)
 
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
-import Msg exposing (Msg(..))
+import Browser.Events
+import Json.Decode as Decode
 import Robot exposing (Direction(..), Model, move, place, turnLeft, turnRight, x, y)
 
 
@@ -11,45 +10,72 @@ type alias Model =
     Robot.Model
 
 
-init : Model
-init =
-    { x = x 0, y = y 0, direction = North }
+type KeyEventMsg
+    = KeyEventLetter Char
+    | KeyEventMeta String
+
+
+type Msg
+    = KeyPressedMsg KeyEventMsg
+
+
+init : flags -> ( Model, Cmd Msg )
+init _ =
+    ( { x = x 0, y = y 0, direction = North }, Cmd.none )
 
 
 main : Program () Model Msg
 main =
-    Browser.sandbox { init = init, update = update, view = view }
+    Browser.document { init = init, update = update, view = view, subscriptions = subscriptions }
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Place x y direction ->
-            place x y direction
+        KeyPressedMsg keyEventMsg ->
+            case keyEventMsg of
+                KeyEventMeta "ArrowLeft" ->
+                    ( turnLeft model, Cmd.none )
 
-        Move ->
-            move model
+                KeyEventMeta "ArrowRight" ->
+                    ( turnRight model, Cmd.none )
 
-        Left ->
-            turnLeft model
+                KeyEventMeta "ArrowUp" ->
+                    ( move model, Cmd.none )
 
-        Right ->
-            turnRight model
+                KeyEventMeta "Escape" ->
+                    init ()
+
+                _ ->
+                    ( model, Cmd.none )
 
 
-view : Model -> Html Msg
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.batch
+        [ Browser.Events.onKeyDown keyPressedDecoder
+        ]
+
+
+view : Model -> Browser.Document Msg
 view model =
-    div []
-        [ viewNav
-        , Robot.view model
+    { title = "ELm Robot Toy"
+    , body =
+        [ Robot.view model
         ]
+    }
 
 
-viewNav : Html Msg
-viewNav =
-    div []
-        [ button [ onClick <| Place 0 0 North ] [ text "init" ]
-        , button [ onClick <| Move ] [ text "move" ]
-        , button [ onClick <| Left ] [ text "left" ]
-        , button [ onClick <| Right ] [ text "right" ]
-        ]
+keyPressedDecoder : Decode.Decoder Msg
+keyPressedDecoder =
+    Decode.map (toKeyEventMsg >> KeyPressedMsg) (Decode.field "key" Decode.string)
+
+
+toKeyEventMsg : String -> KeyEventMsg
+toKeyEventMsg eventKeyString =
+    case String.uncons eventKeyString of
+        Just ( char, "" ) ->
+            KeyEventLetter char
+
+        _ ->
+            KeyEventMeta eventKeyString
